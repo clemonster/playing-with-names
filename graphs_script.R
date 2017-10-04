ba8675cc513809bcb940c4fc19d348a755639c26
clement <clement.ponsonnet@hec.edu>
2017-10-04 16:41:06 +0200

    draw_a_name function plots a graph of the number of people given a name during a year

--- graphs_script.R
+++ graphs_script.R
@@ -6,4 +6,33 @@ library(tidyr)
 data(prenoms)
 
 names(prenoms)
+dim(prenoms)
+View(prenoms)
+head(prenoms)
+summary(prenoms)
 
+#Set some variables to factors
+prenoms <- prenoms %>%
+  mutate_if(is.character, as.factor)
+
+count_name_per_year <- function(the_name, the_sex){
+  
+  result <- prenoms %>% 
+    filter(name == the_name, sex == the_sex) %>%
+    group_by(year) %>%
+    summarise(count = sum(n))
+  return(result)
+}
+
+count_name_per_year("Vincent","M")
+  
+draw_a_name <- function(the_name, the_sex){
+  name_count <- count_name_per_year(the_name,the_sex)
+  plot <- ggplot(data = name_count,
+       aes(x = year, y = count)) +
+    geom_line() 
+  return(plot)
+}
+
+
+draw_a_name(the_name = "Vincent",the_sex = "M")
